3b242ec6b62e397ace6b88a28c143cf2979a9da1
Mehfooz Shaikh <56999250+justtmehfoozz@users.noreply.github.com>
2026-09-05 02:37:59 +0530

    new update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.fivelight.pzxqva"
     minSdk = 26
     targetSdk = 36
-    versionCode = 5
-    versionName = "1.7"
+    versionCode = 6
+    versionName = "1.8"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
